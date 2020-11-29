a07753f5b0dca30b1c170a7e059d356c97455c48
jamiejamiebobamie <jmccrory@vt.edu>
2020-11-29 09:54:50 -0500

    added notes and deployed test server to heroku. react-spring v8 works after deployment.

--- src/websiteExamples/UseGestureTest/Gesture1v8.jsx
+++ src/websiteExamples/UseGestureTest/Gesture1v8.jsx
@@ -1,5 +1,8 @@
 // https://use-gesture.netlify.app/docs/
 
+// react-spring v8: DOES WORK
+// https://react-spring-test.herokuapp.com
+
 import { useSpring, animated } from 'react-spring'
 import { useDrag } from 'react-use-gesture'
 export default function PullRelease() {
@@ -18,3 +21,17 @@ export default function PullRelease() {
     />
   )
 }
+
+// react-spring v9: DOES NOT WORK WHEN DEPLOYED TO THE INTERNET
+
+// import { useSpring, animated } from 'react-spring'
+// import { useDrag } from 'react-use-gesture'
+// function PullRelease() {
+//   const [{ x, y }, set] = useSpring(() => ({ x: 0, y: 0 }))
+//   // Set the drag hook and define component movement based on gesture data
+//   const bind = useDrag(({ down, movement: [mx, my] }) => {
+//     set({ x: down ? mx : 0, y: down ? my : 0 })
+//   })
+//   // Bind it to a component
+//   return <animated.div {...bind()} style={{ x, y }} />
+// }
